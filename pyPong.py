283d08d9e87648f779bfa693392e76f935add68b
JPMorris510 <joshpowellmorris@gmail.com>
2022-05-04 10:28:35 +0100

    Initial commit

---
+++ pyPong.py
@@ -0,0 +1,47 @@
+# Turtle is integral to this program as it will create a separate window
+import turtle as t
+
+# Initialise two int variables for tracking player scores
+'''Player 1 score here'''
+'''Player 2 score here'''
+
+# Using the turtle API, create a variable for the window and call the 'Screen()' function
+"""Your code here"""
+
+# Create the paddle for the left-hand side of the screen
+left_paddle = t.Turtle()
+"""Your code here"""
+
+# Create the paddle for the right-hand side. This code is identical to the left except for one difference.
+right_paddle = t.Turtle()
+"""Your code here"""
+
+# Create the ball
+ball = t.Turtle()
+"""Your code here"""
+
+# Create the scorecard
+score = t.Turtle()
+"""Your code here"""
+
+# Implement code for moving the paddles up and down
+"""Your code here"""
+
+# Game logic
+
+while True:
+
+    # Make the ball move
+    """Your code here"""
+    # Border set up
+    # Make the ball bounce off the top and bottom
+    """Your code here"""
+
+    # Reset the ball and increase the score depending on whose side the ball went off on
+    """Your code here"""
+
+    # Handling the collisions with paddles.
+    """Your code here"""
+
+    # Make the ball bounce off and go in the other direction when it collides with the paddle
+    """Your code here"""
